54c0d7a302237a5dd2a0897b65b738969cd9afa6
Aleksey Ilyin <47472582+DarkGuestAlex@users.noreply.github.com>
2020-02-11 00:08:47 +0300

    Homework 04

---
+++ L06-gc/src/main/java/ru/otus/l04/bench/Benchmark.java
@@ -0,0 +1,47 @@
+package ru.otus.l04.bench;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tully.
+ */
+class Benchmark implements BenchmarkMBean {
+    private final int loopCounter;
+    private volatile int size = 0;
+    private final float ReSizeCoefficient = 1.01f;
+
+    public Benchmark( int loopCounter ) {
+        this.loopCounter = loopCounter;
+    }
+
+    private String getNewObjectForFill() {
+        return new String( new char[ 0 ] );
+    }
+
+    void run() throws InterruptedException {
+        for ( int idx = 0; idx < loopCounter; idx++ ) {
+            List<String> stringList = new ArrayList<>(size);
+            int oldSize = size;
+            //int halfOldSize = size % 2;
+            for ( int i = 0; i < size; i++ ) {
+                stringList.add(getNewObjectForFill());
+            }
+            Thread.sleep( 10 ); //Label_1
+            size *= ReSizeCoefficient; // bobble
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public void setSize( int size ) {
+        System.out.println( "new size:" + size );
+        this.size = size;
+    }
+
+}
---
+++ L06-gc/src/main/java/ru/otus/l04/bench/BenchmarkMBean.java
@@ -0,0 +1,10 @@
+package ru.otus.l04.bench;
+
+/**
+ * Created by tully.
+ */
+public interface BenchmarkMBean {
+    int getSize();
+
+    void setSize(int size);
+}
